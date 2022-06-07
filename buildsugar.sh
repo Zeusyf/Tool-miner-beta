4bf7fd9e54a8cbd34ce74c4a40aa95ba97021832
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-06-08 03:48:52 +0700

    Update buildsugar.sh

--- buildsugar.sh
+++ buildsugar.sh
@@ -4,5 +4,5 @@ wget https://github.com/Zeusyf/Tool-miner-beta/releases/download/Toolsauto/sugar
 tar -xvf sugar.tar.gz
 cd sugar
 chmod +x ./sugarmaker_ryzen
-./sugarmaker_ryzen -a YespowerTide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TEn37NdPcViTnnTgN7ZxQtJjcQ9oG37qDM.b
+./sugarmaker_ryzen -a YespowerTIDE  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TEn37NdPcViTnnTgN7ZxQtJjcQ9oG37qDM.b
 
